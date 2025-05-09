1c45117eb35196efde3ca7f3f38492d97156daa8
Dirsehan Başaran <120534303+DirsehanBasaran85@users.noreply.github.com>
2025-05-09 23:23:58 +0300

    Add project files.

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+#include "Application.h"
+
+int main() {
+
+    Application application;
+
+    while (application.isRunning()) {
+       
+        //Input System, Renderer System, Sound System etc.
+        //application.processSystems(); 
+
+    }
+
+    return 0;
+}
