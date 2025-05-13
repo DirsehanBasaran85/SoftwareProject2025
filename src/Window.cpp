35a02ef75319eeb63f792b6d98ae37737e56128b
izzetcancan <izzetcan65@outlook.com>
2025-05-13 08:17:57 +0300

    Window

--- src/Window.cpp
+++ src/Window.cpp
@@ -2,17 +2,18 @@
 #include "Renderer.h"
 #include <iostream>
 
-Window::Window(unsigned int width, unsigned int height , const std::string& title)
-    : width(width), height(height), title(title)
+Window::Window(unsigned int width, unsigned int height, const std::string& title)
+    : width(width), height(height), title(title), scene({ width,height }), assetManager(100), renderer(window, assetManager, scene)
 {
     createWindow();
-    if (!ImGui::SFML::Init(window)) { // init imgui
+    if (!renderer.getDebugMenu().Init(&window)) { // init imgui
         std::cout << "ImGui failed to start";
     }
+    //ImGui::GetIO().Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Calibri.ttf", 15);
+    //ImGui::GetIO().FontGlobalScale = 1.3f;
 }
 
 Window::~Window() {
-   ImGui::SFML::Shutdown(); // clean up
 }
 
 void Window::run() {
@@ -20,34 +21,76 @@ void Window::run() {
     assetManager.loadTexture("appletex", "assets/AnimationSheet_Character.png", { 0,0 }, { 256,288 });
     Texture tex = assetManager.getTexture("appletex");
     assetManager.loadSprite("apple", tex, { 0,0 }, { 32,32 });
-    scene.addEntity("apple",{570,400});
-    
-    
+
+    scene.addEntity("apple", { 570,400 });
+
+    scene.update();
     windowLoop();
 }
 
 void Window::createWindow() {
 
-    window.create(sf::VideoMode({ width, height }), title,sf::Style::Titlebar | sf::Style::Close);
+    window.create(sf::VideoMode({ width, height }), title, sf::Style::Titlebar | sf::Style::Close);
     window.setVerticalSyncEnabled(true);
 }
 
 void Window::windowLoop() {
-    while (isOpen()) {
 
     while (isOpen())
     {
-        
         while (const std::optional event = window.pollEvent())
         {
-            ImGui::SFML::ProcessEvent(window, *event); // feed imgui
-            
+            renderer.getDebugMenu().ProcessEvent(*event); // feed imgui
+
             if (event->is<sf::Event::Closed>())
                 close();
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+
+                    //window.close();
+                    sf::Vector2f debugtest = scene.getPosition("apple");
+                    std::cout << debugtest.x << std::endl;
+                    std::cout << debugtest.y << std::endl;
+                }
+
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Up) {
+
+                    sf::Vector2f coordinates = scene.getPosition("apple");
+                    coordinates.y -= 10;
+                    scene.setPosition("apple", coordinates);
+                }
+
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Down) {
+
+                    sf::Vector2f coordinates = scene.getPosition("apple");
+                    coordinates.y += 10;
+                    scene.setPosition("apple", coordinates);
+                }
+
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Left) {
+
+                    sf::Vector2f coordinates = scene.getPosition("apple");
+                    coordinates.x -= 10;
+                    scene.setPosition("apple", coordinates);
+                }
+
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Right) {
+
+                    sf::Vector2f coordinates = scene.getPosition("apple");
+                    coordinates.x += 10;
+                    scene.setPosition("apple", coordinates);
+                }
+
+            }
         }
 
-        clear();
 
-        display();
+
+        renderer.render();
+
+
+
     }
 }
